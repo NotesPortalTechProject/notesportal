9cfa9c239ebedf75fb7d745d1508f25698ef37e4
Arhaan Bhiwandkar <arhaanbhiwandkar@gmail.com>
2024-03-16 00:31:18 +0530

    Add files via upload

--- main-new.py
+++ main-new.py
@@ -1,29 +1,24 @@
-from flask import Flask,render_template,redirect,send_file,request,jsonify
-import mysql.connector,random,string,json
+from flask import Flask,render_template,redirect,send_file,request
+import random,string,json
 from datetime import datetime
 from werkzeug.utils import secure_filename
-from docx2pdf import convert
-from pathlib import Path
-from mailsend import sendmail
+# from mailsend import sendmail
+from supabase import create_client
 
 app  = Flask(__name__)
 favoritelist = []
 data = ""
-navsub = []
+navsub = {}
+supabase = create_client("https://pgyqwtttvyezvyyexbfh.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBneXF3dHR0dnllenZ5eWV4YmZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk2MjM0ODIsImV4cCI6MjAyNTE5OTQ4Mn0.Zeajgmmbh8Y2RoeUj4xTi25zQrVlIqGlr7fahbToKak")
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    mydb = mysql.connector.connect(host="localhost",user="root",password="",database="notesportal")
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM users")
-    userslst = mycursor.fetchall()
     id = "".join(random.sample(string.hexdigits,6))
     firstname = ""
     lastname = ""
     username = ""
     mail = ""
     num = 0
-    flag = 0
     subjectlst = []
     if request.method == "POST":
         firstname = request.form["firstname"]
@@ -31,63 +26,45 @@ def index():
         username = request.form["username"]
         mail = request.form["email"]
         num = int(request.form["numsub"])
-        for user in userslst:
-            if user[3] == username:
-                flag+=1
-        if flag == 0:
+        userslst = supabase.table('users').select("*").eq('username',username).execute().data 
+        if len(userslst) == 0:
             try:
                 for x in range(num):
                     subjectlst.append(request.form[f"subject_{x}"].upper())
-                query = "insert into users (id,firstname,lastname,username,email,subjects) values (%s,%s,%s,%s,%s,%s)"
-                mycursor.executemany(query,[(id,firstname,lastname,username,mail,str(subjectlst))])
-                sendmail(username,mail=mail)
-                mydb.commit()
+                #sendmail(username,mail=mail)
+                supabase.table('users').insert({"id": id,"firstname": firstname,"lastname": lastname,"username": username,"email": mail,"subjects": str(subjectlst)}).execute()
                 navtemplst = [id,firstname,lastname,username,mail,str(subjectlst)]
-                for x in range(5):
-                    navsub[x] = navtemplst[x]
+                keylst = ["id","firstname","lastname","username","email","subjects"]
+                for x in range(6):
+                    navsub[keylst[x]] = navtemplst[x]
                 return redirect("/home")
             except KeyError:
                 pass
         else:
             print("User Already Exists")
             return redirect("/signin")
-    mycursor.close() 
     return render_template("index.html",num=num,firstname=firstname,lastname=lastname,username=username,mail=mail)
 
-@app.route("/home>",methods=["POST","GET"])
+@app.route(f"/home",methods=["POST","GET"])
 def home():
     global navsub
     global data
-    mydb = mysql.connector.connect(host="localhost",user="root",password="",database="notesportal")
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM users")
-    userslst = mycursor.fetchall()
-    print(navsub)
-    username = navsub[3]
-    for user in userslst:
-            if user[3] == username:
-                navsub = user
     datalist = []
+    # userslst = supabase.table('users').select("*").eq('subjectname',data).execute().data 
+    # username = navsub["username"]
+    # if not navsub:
+    #     navsub = supabase.table('users').select("*").eq('username',username).execute().data 
+        # for user in userslst:
+        #     if user["username"] == username:
+        #             navsub = user
+        #             print(navsub)
+        # print(navsub)
     if request.method == "POST":
-        mydb = mysql.connector.connect(host="localhost",user="root",password="",database="notesportal")
-        mycursor = mydb.cursor()
         try:
             data = request.form["navbarsubject"]
         except KeyError:
             pass
-        mycursor.execute("SELECT * FROM notes")
-        fetchdata = mycursor.fetchall()
-        for element in fetchdata:
-            if data == element[3] and data != "":
-                eledict = {}
-                eledict["id"] = element[0]
-                eledict["date"] = element[1]
-                eledict["filename"] = element[2]
-                eledict["subjectname"] = element[3]
-                eledict["filetype"] = element[4]
-                eledict["filelink"] = element[5]
-                print("found")
-                datalist.append(eledict)
+        datalist = supabase.table('notes').select('*').eq('subjectname',data).execute().data
         choosefile = request.form.get("pdfbutton")
         if choosefile:
             choosefile = choosefile.replace("\'","\"")
@@ -102,13 +79,10 @@ def home():
             favorite = favorite.replace("\'","\"")
             favorite = json.loads(favorite)
             favoritelist.remove(favorite)
-        mycursor.close()
-    return render_template("home.html",navsublst=eval(navsub[5]),pdflist=datalist,favoritelist=favoritelist)
+    return render_template("home.html",navsublst=eval(navsub["subjects"]),pdflist=datalist,favoritelist=favoritelist)
 
 @app.route("/upload",methods=["POST","GET"])
 def upload():
-    mydb = mysql.connector.connect(host="localhost",user="root",password="",database="notesportal")
-    mycursor = mydb.cursor()
     if request.method == "POST":
         file = request.files["file"]
         subject = request.form["subject"].upper()
@@ -119,18 +93,7 @@ def upload():
         filename = secure_filename(subject+"_"+topic+"."+filetype)
         filelink = f"static/database/{filename}"
         file.save(filelink)
-        if filetype == "doc" or filetype == "docx":
-            convert(f"static/database/{filename}",f"static/database/{filename.split('.')[0] + '.pdf'}")
-            Path(f"static/database/{filename}").unlink()
-            filename = filename.split(".")[0] + ".pdf"
-            filelink = f"static/database/{filename}"
-            query = "insert into notes (id,date,filename,subjectname,filetype,filelink) values (%s,%s,%s,%s,%s,%s)"
-            mycursor.executemany(query,[(id,date,filename,subject,"pdf",filelink)])
-        else:
-            query = "insert into notes (id,date,filename,subjectname,filetype,filelink) values (%s,%s,%s,%s,%s,%s)"
-            mycursor.executemany(query,[(id,date,filename,subject,filetype,filelink)])
-        mydb.commit()
-        mycursor.close()
+        supabase.table('notes').insert({"id": id,"date": date,"filename": filename,"subjectname": subject,"filetype": filetype,"filelink": filelink}).execute()
     return render_template("upload.html")
 
 @app.route("/favorites",methods=["POST","GET"])
@@ -145,17 +108,13 @@ def favorites():
         unfavorite = unfavorite.replace("\'","\"")
         unfavorite = json.loads(unfavorite)
         favoritelist.remove(unfavorite)
-        print(favoritelist)
     return render_template("favorites.html",favoritelist=favoritelist)
 
 @app.route("/addsubjects",methods=["POST","GET"])
 def addsubject():
-    mydb = mysql.connector.connect(host="localhost",user="root",password="",database="notesportal")
-    mycursor = mydb.cursor()
     num=0
-    tempnavsub = eval(navsub[5])
-    username = navsub[3]
-    query = "UPDATE `users` SET `subjects`=%s WHERE `username`=%s"
+    tempnavsub = eval(navsub["subjects"])
+    username = navsub["username"]
     if request.method == "POST":
         num = int(request.form["numsub"]) 
         try:
@@ -164,30 +123,24 @@ def addsubject():
                 print(subject)
                 if subject != "" and subject != " " and len(subject)!=0 and subject not in tempnavsub:
                     tempnavsub.append(subject)
-            mycursor.execute(query,(str(tempnavsub),username))
-            mydb.commit() 
+            navsub["subjects"] = str(tempnavsub)
+            supabase.table('users').update({'subjects':tempnavsub}).eq('username',username).execute()
         except KeyError:
             pass
     return render_template("addsubject.html",tempnavsub=tempnavsub,num=num)
 
 @app.route("/removesubjects",methods=["POST","GET"])
 def removesubject():
-    mydb = mysql.connector.connect(host="localhost",user="root",password="",database="notesportal")
-    mycursor = mydb.cursor()
     num=0
-    tempnavsub = eval(navsub[5])
-    username = navsub[3]
-    query = "UPDATE `users` SET `subjects`=%s WHERE `username`=%s"
+    tempnavsub = eval(navsub["subjects"])
+    username = navsub["username"]
     if request.method == "POST":
         editsub = request.form.get("editsubject")
-        print(editsub)
         try:
-            print("code at remove")
             tempnavsub.remove(editsub)
-            mycursor.execute(query,(str(tempnavsub),username))
-            print("remove "+str(tempnavsub))
-            navsub[5] = tempnavsub
-            mydb.commit()
+            supabase.table('users').update({'subjects':tempnavsub}).eq('username',username).execute()
+            navsub["subjects"] = str(tempnavsub)
+            print(tempnavsub)
         except ValueError:
             pass
     return render_template("removesubjects.html",tempnavsub=tempnavsub,num=num)
@@ -195,17 +148,12 @@ def removesubject():
 @app.route("/signin",methods=["POST","GET"])
 def signin():
     global navsub
-    mydb = mysql.connector.connect(host="localhost",user="root",password="",database="notesportal")
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM users")
-    userslst = mycursor.fetchall()
     if request.method == "POST":
         username = request.form["username"]
-        for user in userslst:
-                if user[3] == username:
-                    navsub = user
-                    print(user)
-                    return redirect("/home")
+        tempdict = supabase.table('users').select("*").eq('username',username).execute().data[0]
+        for i,j in list(tempdict.items()):
+            navsub[i] = j
+        return redirect("/home")
     return render_template("signin.html")
 
 if "__main__" == __name__:
